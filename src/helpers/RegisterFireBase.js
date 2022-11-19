e03528fef64ee6f90635362f6fd705a12ec98cd4
MeralSavas <meralsavas@gmail.com>
2022-11-19 11:58:34 +0000

    registerfirebase

--- src/helpers/RegisterFireBase.js
+++ src/helpers/RegisterFireBase.js
@@ -1,18 +1,36 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 
-function RegisterFireBase() {
-  const auth = getAuth(email, password);
+function RegisterFireBase(email, password, data, setError, navigate) {
+  const auth = getAuth();
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      // ...
+      Update(data);
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
+      setError(error.code);
       // ..
     });
 }
 
 export default RegisterFireBase;
+const Update = () => {
+  const auth = getAuth();
+  updateProfile(auth.currentUser, {
+    displayName: "Jane Q. User",
+    photoURL: "https://example.com/jane-q-user/profile.jpg",
+  })
+    .then(() => {
+      // Profile updated!
+      // ...
+    })
+    .catch((error) => {
+      // An error occurred
+      // ...
+    });
+};
