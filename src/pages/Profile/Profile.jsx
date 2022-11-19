29a1fd00fa04817a6da2bb44cfb216f21ceee0f3
MeralSavas <meralsavas@gmail.com>
2022-11-19 09:10:48 +0000

    profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { PhotoDiv, ProfileCon, ProfileInfo } from "./Profile-styled";
 
-const Profile = () => {
-  return <div>Profile</div>;
-};
+function Profile() {
+  return (
+    <div>
+      <ProfileCon>
+        <PhotoDiv></PhotoDiv>
+        <ProfileInfo>
+          <p>name</p>
+          <p>gender</p>
+          <p>age</p>
+          <p>address</p>
+        </ProfileInfo>
+      </ProfileCon>
+    </div>
+  );
+}
 
 export default Profile;
